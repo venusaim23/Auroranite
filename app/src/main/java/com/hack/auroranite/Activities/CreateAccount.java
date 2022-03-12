893683ce2e6a31e9b6189663c8525923c83b21b7
Venu Sai <venusaim23@gmail.com>
2022-03-12 21:12:26 +0530

    add login and sign up screens

---
+++ app/src/main/java/com/hack/auroranite/Activities/CreateAccount.java
@@ -0,0 +1,103 @@
+package com.hack.auroranite.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.hack.auroranite.Models.User;
+import com.hack.auroranite.R;
+import com.hack.auroranite.databinding.ActivityCreateAccountBinding;
+
+public class CreateAccount extends AppCompatActivity {
+
+    private ActivityCreateAccountBinding binding;
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private DatabaseReference dbRef;
+
+    private String[] modes;
+    private User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityCreateAccountBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        if (mUser != null)
+            finish();
+
+        dbRef = FirebaseDatabase.getInstance().getReference("Users");
+
+        modes = getResources().getStringArray(R.array.work_mode);
+
+        binding.createBtnSu.setOnClickListener(v -> getDetails());
+    }
+
+    private void getDetails() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(binding.signUpLayout.getWindowToken(), 0);
+
+        binding.progressBarSignUp.setVisibility(View.VISIBLE);
+
+        String name = binding.nameEtSu.getText().toString().trim();
+        String email = binding.emailEtSu.getText().toString().trim();
+        String password = binding.passwordEtSu.getText().toString();
+        String repPassword = binding.repPasswordEtSu.getText().toString();
+        String education = binding.educationEtSu.getText().toString().trim();
+        String major = binding.majorEtSu.getText().toString().trim();
+        String gradYear = binding.gradYearEtSu.getText().toString().trim();
+        String country = binding.countryEtSu.getText().toString().trim();
+        String skills = binding.skillsEtSu.getText().toString().trim();
+        String experience = binding.expEtSu.getText().toString().trim();
+        String salary = binding.salaryEtSu.getText().toString().trim();
+        String mode = modes[binding.spinnerPreferredModeSu.getSelectedItemPosition()];
+
+        user = new User(mUser.getUid(), name, email, education, major, gradYear, country,
+                skills, experience, salary, mode);
+
+        if (password.equals(repPassword))
+            createAccount(email, password);
+    }
+
+    private void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(CreateAccount.this, "Account created", Toast.LENGTH_SHORT).show();
+                        mUser = mAuth.getCurrentUser();
+                        updateDetails();
+                    } else {
+                        Toast.makeText(CreateAccount.this, "Account could not be created: "
+                                + task.getException(), Toast.LENGTH_LONG).show();
+                        binding.progressBarSignUp.setVisibility(View.GONE);
+                    }
+                });
+    }
+
+    private void updateDetails() {
+        dbRef.child(user.getUID()).setValue(user).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Toast.makeText(CreateAccount.this, "Details saved", Toast.LENGTH_SHORT).show();
+            } else {
+                //handle error - prompt user to re-upload details after logging
+                Toast.makeText(CreateAccount.this, "Details could not be saved: "
+                        + task.getException(), Toast.LENGTH_LONG).show();
+            }
+            binding.progressBarSignUp.setVisibility(View.GONE);
+//            startActivity(new Intent(CreateAccount.this, HomeScreen.class));
+            finish();
+        });
+    }
+}
--- app/src/main/java/com/hack/auroranite/Activities/LoginActivity.java
+++ app/src/main/java/com/hack/auroranite/Activities/LoginActivity.java
@@ -1,9 +1,25 @@
 package com.hack.auroranite.Activities;
 
+import static com.hack.auroranite.Util.Utility.layoutErrorManager;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.hack.auroranite.R;
 import com.hack.auroranite.databinding.ActivityLoginBinding;
 
@@ -11,10 +27,81 @@ public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
 
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.signInBtn.setOnClickListener(v -> login());
+
+        binding.createAccountTv.setOnClickListener(v -> startActivity(new Intent(this, CreateAccount.class)));
+
+        binding.passwordEtLogin.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_SEND) {
+                login();
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private void login() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(binding.loginLayout.getWindowToken(), 0);
+
+        binding.progressBarLogin.setVisibility(View.VISIBLE);
+
+        String email = binding.emailEtLogin.getText().toString().trim();
+        String password = binding.passwordEtLogin.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            layoutErrorManager(binding.emailTilLogin, binding.emailEtLogin, "Required");
+            binding.progressBarLogin.setVisibility(View.GONE);
+            return;
+        }
+
+        if (password.isEmpty()) {
+            layoutErrorManager(binding.passwordTilLogin, binding.passwordEtLogin, "Required");
+            binding.progressBarLogin.setVisibility(View.GONE);
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                mUser = mAuth.getCurrentUser();
+                displayHome();
+            } else {
+                Toast.makeText(LoginActivity.this, "Sign in failed: " +
+                        task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                binding.progressBarLogin.setVisibility(View.GONE);
+            }
+        }).addOnFailureListener(e -> {
+            binding.progressBarLogin.setVisibility(View.GONE);
+            Toast.makeText(LoginActivity.this, "Sign in failed: " + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mUser = mAuth.getCurrentUser();
+        if (mUser != null)
+            displayHome();
+    }
+
+    private void displayHome() {
+        binding.progressBarLogin.setVisibility(View.GONE);
+        Toast.makeText(this, "Logged in", Toast.LENGTH_SHORT).show();
+//        startActivity(new Intent(this, HomeScreen.class));
+        finish();
     }
 }
